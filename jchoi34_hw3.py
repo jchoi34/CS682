a132e2f65b04ab1be222a26a60346446a209c389
jchoi34 <jchoi34@gmu.edu>
2020-02-24 00:02:23 -0500

    Update jchoi34_hw3.py

--- jchoi34_hw3.py
+++ jchoi34_hw3.py
@@ -25,18 +25,27 @@ img = cv2.imread('../images/ST2MainHall4/ST2MainHall4001.jpg', 1)
 blues = img[:, :, 0]
 greens = img[:, :, 1]
 reds = img[:, :, 2]
-gray_edges = cv2.Canny(gray_img, 100, 200).astype(bool)
-blue_edges = cv2.Canny(blues, 100, 200)
-red_edges = cv2.Canny(greens, 100, 200)
-green_edges = cv2.Canny(reds, 100, 200)
-sobelX = np.ma.array(cv2.Sobel(gray_img,cv2.CV_64F,1,0,ksize=5), mask=gray_edges).filled(fill_value=0)
-sobelY = np.ma.array(cv2.Sobel(gray_img,cv2.CV_64F,0,1,ksize=5), mask=gray_edges).filled(fill_value=0)
-# sobelX = np.array([sobelX[i] if gray_edges[i] != 0 else 0 for i in range(np.shape(gray_edges)[0])])
-# sobelY = np.array([sobelY[i] if gray_edges[i] != 0 else 0 for i in range(np.shape(gray_edges)[0])])
-print(cv2.Sobel(gray_img,cv2.CV_64F,1,0,ksize=5))
-print()
-print(sobelX)
-# cv2.imshow('img', sobelX)
-# cv2.imshow('edges', sobelY)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+gray_edges = cv2.Canny(gray_img, 100, 200) == 0
+blue_edges = cv2.Canny(blues, 100, 200) == 0
+red_edges = cv2.Canny(reds, 100, 200) == 0
+green_edges = cv2.Canny(greens, 100, 200) == 0
+sobelX_gray = cv2.Sobel(gray_img,cv2.CV_64F,1,0,ksize=5)
+sobelY_gray = cv2.Sobel(gray_img,cv2.CV_64F,0,1,ksize=5)
+sobelX_blue = cv2.Sobel(blues,cv2.CV_64F,1,0,ksize=5)
+sobelY_blue = cv2.Sobel(blues,cv2.CV_64F,0,1,ksize=5)
+sobelX_red = cv2.Sobel(reds,cv2.CV_64F,1,0,ksize=5)
+sobelY_red = cv2.Sobel(reds,cv2.CV_64F,0,1,ksize=5)
+sobelX_green = cv2.Sobel(greens,cv2.CV_64F,1,0,ksize=5)
+sobelY_green = cv2.Sobel(greens,cv2.CV_64F,0,1,ksize=5)
+sobelX_gray[gray_edges] = 0
+sobelY_gray[gray_edges] = 0
+sobelX_blue[blue_edges] = 0
+sobelY_blue[blue_edges] = 0
+sobelX_red[red_edges] = 0
+sobelY_red[red_edges] = 0
+sobelX_green[green_edges] = 0
+sobelY_green[green_edges] = 0
+cv2.imshow('img', sobelX_gray)
+cv2.imshow('edges', sobelX_gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
